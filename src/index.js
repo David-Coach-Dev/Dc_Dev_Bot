eb280c40217a2dbac5b787e9305a4a74ea807b75
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-01-23 21:12:35 -0500

    act

--- src/index.js
+++ src/index.js
@@ -1,60 +1,20 @@
-require('https').createServer((req, res) => res.end('Dc Dev Bot En Linea...')).listen(3000);
+const express = require("express");
+const path = require("path");
+const http = require("http");
+//initialize
+const app = express();
+const p = 3000;
+const server = http.createServer(app);
+//settings
+app.set("port",  p);
+//middleware
+require("./Cargar/bot");
+
+//routes
+
+//static files
 app.use(express.static("public"));
-const {
-  Client,
-  Intents,
-  GatewayIntentBits,
-  Collection,
-} = require("discord.js");
-const config = require("./config/config.json");
-const { join } = require("path");
-const { setInterval } = require("timers");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+//starting server
+server.listen(app.get("port"), () => {
+  console.log("server on port ", p, " http://localhost:"+p);
 });
-
-client.commands = new Collection();
-client.languages = require("i18n");
-client.languages.configure({
-  locales: ["es", "en"],
-  directory: join(__dirname, "languages"),
-  defaultLocale: "es",
-  objectNotation: true,
-  register: global,
-  logWarnFn: function (msg) {
-    console.warn("WARNING", msg);
-  },
-  logErrorFn: function (msg) {
-    console.error("ERROR", msg);
-  },
-  missingKeyFn: function (locale, value) {
-    return value;
-  },
-  mustacheConfig: {
-    tags: ["{{", "}}"],
-    disable: false,
-  },
-});
-client.languages.setLocale("es");
-
-setInterval(function () {
-  updateStatus();
-}, 60000);
-
-async function updateStatus() {
-  const guidNum = client.guilds.cache.size;
-  const memberNum = client.guilds.cache.reduce((prev, guild) => prev + guild.memberCount, 0);
-  await client.user.setActivity(
-    `${guidNum} servidores y ${memberNum} miembros`,
-    { type: "LISTENING" }
-  );
-};
-require("../src/handlers/events.js")(client);
-require("../src/handlers/commands.js")(client);
-client.login(config.token);
