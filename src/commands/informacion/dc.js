f80b8d8be4d7792eeec15933bf9df62510c251a3
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-01-24 10:19:00 -0500

    act

--- src/commands/informacion/dc.js
+++ src/commands/informacion/dc.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
-const config = require("../../config/config.json");
+const { join } = require("path");
+require("dotenv").config({ path: join(__dirname, "../config/.env") });
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,17 +9,16 @@ module.exports = {
     .setDescription("Portafolio de Dc Dev!"),
   async run(client, interaction) {
     const embed = new EmbedBuilder()
-      .setColor(config.defaultErrorColor)
+      .setColor(process.env.DEFAULT_ERROR_COLOR)
       .setTitle(client.languages.__("dc.title"))
       .setDescription(`${client.languages.__("dc.msn")}`)
       .setThumbnail("https://i.imgur.com/iBYouzb.png")
       .setImage("https://i.imgur.com/yysg2eN.png")
       .setFooter({
-        text:
-          client.languages.__mf("dc.footer", {
-            time: Date.now() - interaction.createdTimestamp,
-            username: interaction.user.username,
-          }),
+        text: client.languages.__mf("dc.footer", {
+          time: Date.now() - interaction.createdTimestamp,
+          username: interaction.user.username,
+        }),
         iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
       })
       .setTimestamp();
